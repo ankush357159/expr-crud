d77551954980bb2289b15691737300bf2f2ac7a0
Ankush <ankush357159@gmail.com>
2022-06-19 08:35:13 +0530

    update user

--- app/controller/User.js
+++ app/controller/User.js
@@ -29,3 +29,69 @@ exports.create = async (req, res) => {
       res.status(404).json({ message: error.message });
     });
 };
+
+// Retrieve all users from the database
+exports.findAll = async (req, res) => {
+  try {
+    const user = await UserModel.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// Find a single user by Id
+exports.findOne = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// Update the user by Id
+exports.update = async (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: "Data to update can not be empty",
+    });
+  }
+  const id = req.params.id;
+  await UserModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: "User not found",
+        });
+      } else {
+        res.send({ message: "User updated successfully." });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message: error.message,
+      });
+    });
+};
+
+// Delete a user with userId
+exports.destroy = async (req, res) => {
+  await UserModel.findByIdAndRemove(req.params.id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: "User not found",
+        });
+      } else {
+        res.send({
+          message: "User deleted successfully",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message: error.message,
+      });
+    });
+};
--- app/routes/User.js
+++ app/routes/User.js
@@ -5,6 +5,10 @@ const UserController = require('../controller/User')
 const router = express.Router()
 
 router.post('/create', UserController.create)
+router.get('/findusers', UserController.findAll)
+router.get('/finduser/:id', UserController.findOne)
+router.put('/updateuser/:id', UserController.update)
+router.delete('/deleteuser/:id', UserController.destroy)
 
 
 module.exports = router
